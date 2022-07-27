833e5d281f6872f05fda61a194a32258c644e569
Aldo Rodrigo Hernandez <aldo.hernandez@cmgo.org.mx>
2022-07-27 15:22:28 -0500

    config table instructors

--- db/instructors.sql
+++ db/instructors.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 18-07-2022 a las 20:57:58
+-- Tiempo de generación: 27-07-2022 a las 20:21:21
 -- Versión del servidor: 5.7.36
 -- Versión de PHP: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,23 +30,23 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `instructors`;
 CREATE TABLE IF NOT EXISTS `instructors` (
   `id_instructor` int(11) NOT NULL AUTO_INCREMENT,
-  `name_instructor` varchar(100) NOT NULL,
-  `username_instructor` varchar(100) NOT NULL,
+  `name_instructor` varchar(100) DEFAULT NULL,
+  `username_instructor` varchar(100) DEFAULT NULL,
   `email_instructor` varchar(70) NOT NULL,
-  `password_instructor` varchar(50) NOT NULL,
-  `token_instructor` varchar(150) NOT NULL,
+  `password_instructor` varchar(150) NOT NULL,
+  `token_instructor` varchar(150) DEFAULT NULL,
+  `token_exp_instructor` varchar(150) DEFAULT NULL,
+  `date_created_instructor` date DEFAULT NULL,
+  `date_update_instructor` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_instructor`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `instructors`
 --
 
-INSERT INTO `instructors` (`id_instructor`, `name_instructor`, `username_instructor`, `email_instructor`, `password_instructor`, `token_instructor`) VALUES
-(1, 'Aldo Rodrigo Hernandez Lopez', 'aldorodrigo27', 'aldo1rodrigo@gmail.com', '123456', ''),
-(2, 'Fernando Rubio Rivera', 'fernandoriverar', 'sasukerivera@gmail.com', '123456789', ''),
-(3, 'Claudia Flores Moreno', 'claudiaflores', 'caludiaflores@gmail.com', '123456', ''),
-(4, 'Victor Marquez Moncada', 'victormm', 'victormm@gmail.com', '123456789', '');
+INSERT INTO `instructors` (`id_instructor`, `name_instructor`, `username_instructor`, `email_instructor`, `password_instructor`, `token_instructor`, `token_exp_instructor`, `date_created_instructor`, `date_update_instructor`) VALUES
+(1, NULL, NULL, 'aldo1rodrigo@cmgo.org.mx', '$2a$07$Cam8h88K3NHemXNGPql1cukslZNik3BxJJByrGNwuazlgP59MY4AS', NULL, NULL, NULL, '2022-07-27 18:25:04');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
